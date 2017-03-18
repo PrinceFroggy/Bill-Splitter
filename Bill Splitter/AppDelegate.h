063a092ad4006a0ab1241ac755da2dcf2e7ad040
PrinceFroggy <asolesa@yahoo.ca>
2017-03-18 14:25:13 -0400

    Initial Commit

---
+++ Bill Splitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bill Splitter
+//
+//  Created by Andrew Solesa on 2017-03-18.
+//  Copyright © 2017 KSG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
